14a16aa1c835b7d7ba074b4e20697153226fc407
evan999 <emeshberge@gmail.com>
2020-11-04 23:54:44 -0500

    Bank methods: addAccount, createAccount, getClient.

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -5,7 +5,7 @@ import BankAccount.BankAccount;
 
 import java.util.HashMap;
 
-public class Bank extends BankAccount {
+public class Bank {
     private String name;
     private int safeAmt;
     private HashMap<String, Client> clients;
@@ -23,18 +23,45 @@ public class Bank extends BankAccount {
 
     
 
-    public Account createAccount(){
-
+    public Account createAccount(String accountType, Client client, int accountId, int startingBalance){
+        switch(accountType){
+            case "checking":
+                return new CheckingAccount(accountId, startingBalance, client);
+            case "savings":
+                int interest = ui.requestInt("Please enter interest rate:");
+                return new SavingsAccount(accountId);
+            default:
+                return null;
+        }
     };
+
+    public void displayClients(){
+        for (var client : clients.keySet()){
+            System.out.println(clients.get(client));
+        }
+    }
+
+    public void addClient(String firstName, String lastName){
+        clientCount += 1;
+        clients.put(clientCount.toString(), new Client(firstName, lastName, clientCount.toString()));
+    }
 
     public void getClient(String clientId){
 
     }
 
+    public Account getAccount(int accountId){
+        return accounts.get(accountId);
+    }
+
     // Minimum: Come up with
-    public void addCheckingAccount(){
+    public void addAccount(String clientId, String accountType, int startingBalance, UI ui){
         // TODO: get client
         // TODO: create account giving the client.
         // Which account are we creating?
+        Client client = clients.get(clientId);
+        Account newAccount = createAccount(accountType, client, ++accountCount, startingBalance, ui);
+        client.addAccount(newAccount);
+
     }
 }
