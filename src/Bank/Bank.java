432c81af60d990b7efe2eaf6c7a9ea547c36e154
evan999 <emeshberge@gmail.com>
2020-11-05 23:04:04 -0500

    Update Bank, Chaos, Console, and UI classes

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -21,9 +21,32 @@ public class Bank {
 
     // TODO: Best way to add an account
 
-    
+    public void addClient(String firstName, String lastName){
+        clientCount += 1;
+        clients.put(clientCount.toString(), new Client(firstName, lastName, clientCount.toString()));
+    }
 
-    public Account createAccount(String accountType, Client client, int accountId, int startingBalance){
+    public void getClient(String clientId){
+
+    }
+
+    // Minimum: Come up with
+    public void addAccount(String clientId, String accountType, int startingBalance, UI ui){
+        // TODO: get client
+        // TODO: create account giving the client.
+        // Which account are we creating?
+
+        // Get client from client collection
+        Client client = clients.get(clientId);
+
+        // Create account giving it the client.
+        Account newAccount = createAccount(accountType, client, ++accountCount, startingBalance, ui);
+        // Attach to accounts collection
+        client.addAccount(newAccount);
+        accounts.put(accountCount, newAccount);
+    }
+
+    public Account createAccount(String accountType, Client client, int accountId, int startingBalance, UI ui){
         switch(accountType){
             case "checking":
                 return new CheckingAccount(accountId, startingBalance, client);
@@ -41,27 +64,9 @@ public class Bank {
         }
     }
 
-    public void addClient(String firstName, String lastName){
-        clientCount += 1;
-        clients.put(clientCount.toString(), new Client(firstName, lastName, clientCount.toString()));
-    }
-
-    public void getClient(String clientId){
-
-    }
-
     public Account getAccount(int accountId){
         return accounts.get(accountId);
     }
 
-    // Minimum: Come up with
-    public void addAccount(String clientId, String accountType, int startingBalance, UI ui){
-        // TODO: get client
-        // TODO: create account giving the client.
-        // Which account are we creating?
-        Client client = clients.get(clientId);
-        Account newAccount = createAccount(accountType, client, ++accountCount, startingBalance, ui);
-        client.addAccount(newAccount);
 
-    }
 }
